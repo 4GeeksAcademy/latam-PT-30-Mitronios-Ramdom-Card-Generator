29620809f28a8fbf951ee4bd4421c68abef10450
Miguel Angel <160284274+Mitronios@users.noreply.github.com>
2024-04-25 01:31:32 +0000

    Random card Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomSuit = generateRandomSuit();
+  const card = document.querySelector("#card");
+  if (randomSuit == "♠️" || randomSuit == "♣️") {
+    card.classList.replace("text-danger", "text-dark");
+  }
+  document.querySelector("#topSuit").innerHTML = randomSuit;
+  document.querySelector("#number").innerHTML = generateCardNumber();
+  document.querySelector("#bottomSuit").innerHTML = randomSuit;
+};
+
+const generateCardNumber = () => {
+  const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  return randomNumber;
+};
+
+const generateRandomSuit = () => {
+  const suits = ["♦️", "♥️", "♠️", "♣️"];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  return randomSuit;
 };
